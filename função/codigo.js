f00c9122cabad09477a3925bf4669c9170958463
SamuelS <samucalexx@gmail.com>
2025-05-27 11:41:34 -0300

    funções 27/05/2025

--- função/codigo.js
+++ função/codigo.js
@@ -1,1 +1,23 @@
-ECHO est  ativado.
+console.log("Faça seu login")
+let usuario = prompt("Digite seu login: ")
+let senha = prompt("Digite sua senha: ")
+
+if (usuario === "admin" && senha === "admin"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user1" && senha === "user1"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user2" && senha === "user2"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user3" && senha === "user3"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user4" && senha === "user4"){
+    console.log("Login efetuado com sucesso!")
+}
