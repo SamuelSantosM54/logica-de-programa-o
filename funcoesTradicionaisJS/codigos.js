07c0c69ace6a42fc166087dc377694b6e8130129
SamuelS <samucalexx@gmail.com>
2025-06-03 10:58:49 -0300

    codigos.js

---
+++ funcoesTradicionaisJS/codigos.js
@@ -0,0 +1,116 @@
+function OlaMundo() {
+console.log(&quot;Olá, mundo&quot;)
+}
+OlaMundo()
+
+function MostrarNome() {
+console.log(&quot;Samuel Santos Moreira&quot;)
+}
+MostrarNome()
+
+function MostrarIdade() {
+console.log(&quot;16&quot;)
+}
+MostrarIdade()
+
+function SomarDoisNumeros() {
+let resultado = 10 + 11
+console.log(&quot;Resultado da soma:&quot;, resultado)
+}
+SomarDoisNumeros()
+
+function MostrarMensagem() {
+console.log(&quot;Força, foco e fé&quot;)
+}
+MostrarMensagem()
+
+function MostrarData() {
+console.log(&quot;06/05/2025&quot;)
+}
+MostrarData()
+
+function MostrarCidade() {
+console.log(&quot;Ferraz de Vasconcelos&quot;)
+}
+MostrarCidade()
+
+function BemVindo() {
+let nome = prompt(&quot;Digite seu nome: &quot;)
+console.log(&quot;Bem-vindo, &quot; + nome + &quot;!&quot;)
+}
+BemVindo()
+
+function Subtraçao(num1, num2) {
+console.log(&quot;Resultado da subtração:&quot;, num1 - num2)
+}
+Subtraçao(40, 9)
+
+function MostrarAnimalFavorito() {
+console.log(&quot;Cachorro&quot;)
+}
+MostrarAnimalFavorito()
+
+function Aprendendo() {
+console.log(&quot;Estou aprendendo JavaScript!&quot;)
+}
+Aprendendo()
+
+function TriploDoNumero() {
+let numero = prompt(&quot;Digite um número: &quot;)
+let triploValor = Number(numero) * 3
+console.log(&quot;O triplo de &quot; + numero + &quot; é &quot; + triploValor)
+}
+TriploDoNumero()
+
+function digaSeuNomeEIdade() {
+
+console.log(&quot;Meu nome é Samuel e tenho 16 anos&quot;)
+}
+digaSeuNomeEIdade()
+
+function mostreNumeros() {
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+
+}
+mostreNumeros()
+
+function inicioDaAula() {
+console.log(&quot;A aula de funções começou!&quot;)
+}
+inicioDaAula()
+
+function numeroDigitadoFoi() {
+let numero = prompt(&quot;Digite um número: &quot;)
+console.log(&quot;Você digitou o número: &quot; + numero )
+}
+numeroDigitadoFoi();
+
+function nomeDoCursoAtual() {
+let oCurso = &quot;Lógica de Programação&quot;;
+console.log (oCurso)
+}
+nomeDoCursoAtual();
+
+function fazerLigacao() {
+console.log(&quot;Toc toc... Alô, é o Samuel&quot;)
+}
+fazerLigacao(&quot;Samuel&quot;);
+
+function minhaCorPreferida() {
+console.log(&quot;Minha cor favorita é Preto&quot;)
+}
+minhaCorPreferida();
+
+function ondeEuMoro() {
+console.log(&quot;Eu moro no Brasil&quot;)
+}
+ondeEuMoro();
