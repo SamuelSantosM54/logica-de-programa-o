a4c865834006cbab2def7253d67980bc9b2173bf
SamuelS <samucalexx@gmail.com>
2025-06-03 10:43:41 -0300

    codigos.js

---
+++ 10perguntasProgramiz/codigos.js
@@ -0,0 +1,157 @@
+let respostaCorreta = 0
+let respostaIncorreta = 0
+
+//Pergunta1
+console.log("Qual maior país do mundo? ")
+console.log("A - Rússia")
+console.log("B - Canadá")
+console.log("C - China")
+console.log("D - EUA")
+let pergunta1 = prompt("Digite a alternativa correta: ")
+if (pergunta1 == "A" || pergunta1 == "Rússia"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Pergunta2
+console.log("Qual maior cidade do mundo? ")
+console.log("A - Xangai")
+console.log("B - Tóquio")
+console.log("C - São Paulo")
+console.log("D - Malina")
+let pergunta2 = prompt("Digite a alternativa correta: ")
+if (pergunta2 == "B" || pergunta2 == "Tóquio"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Pergunta3
+console.log("Qual maior animal do mundo? ")
+console.log("A - Avestruz")
+console.log("B - Salamandra")
+console.log("C - Crocodilo")
+console.log("D - Baleia azul")
+let pergunta3 = prompt("Digite a alternativa correta: ")
+if (pergunta3 == "D" || pergunta3 == "Baleia azul"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Pergunta4
+console.log("Qual animal mais pesado do mundo? ")
+console.log("A - Sucuri")
+console.log("B - Baleia azul")
+console.log("C - Girafa")
+console.log("D - Urso Polar")
+let pergunta4 = prompt("Digite a alternativa correta: ")
+if (pergunta4 == "B" || pergunta4 == "Baleia azul"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Pergunta5
+console.log("Qual animal mais lento do mundo? ")
+console.log("A - Lesma")
+console.log("B - Coala")
+console.log("C - Tartaruga")
+console.log("D - Bicho-Preguiça")
+let pergunta5 = prompt("Digite a alternativa correta: ")
+if (pergunta5 == "D" || pergunta5 == "Bicho-Preguiça"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Pergunta6
+console.log("Qual animal mais calmo do mundo? ")
+console.log("A - Panda")
+console.log("B - Bicho-preguiça")
+console.log("C - Leopardo")
+console.log("D - Golden retriever")
+let pergunta6 = prompt("Digite a alternativa correta: ")
+if (pergunta6 == "B" || pergunta6 == "Bicho-preguiça"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Pergunta7
+console.log("Qual animal mais Bravo do mundo? ")
+console.log("A - Tubarão Branco")
+console.log("B - Leão")
+console.log("C - Escorpião")
+console.log("D - Búfalo")
+let pergunta7 = prompt("Digite a alternativa correta: ")
+if (pergunta7 == "A" || pergunta7 == "Tubarão Branco"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Pergunta8
+console.log("Qual animal mais inteligente do mundo? ")
+console.log("A - Golfinhos")
+console.log("B - Papagaios")
+console.log("C - Porcos")
+console.log("D - Chimpanzés")
+let pergunta8 = prompt("Digite a alternativa correta: ")
+if (pergunta8 == "D" || pergunta8 == "Chimpanzés"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Pergunta9
+console.log("Qual animal mais rápido do mundo? ")
+console.log("A - Gazela")
+console.log("B - Peixe-agulhão")
+console.log("C - Falcão-peregrino")
+console.log("D - Guepardo")
+let pergunta9 = prompt("Digite a alternativa correta: ")
+if (pergunta9 == "C" || pergunta9 == "Falcão-peregrino"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Pergunta10
+console.log("Qual animal mais caro do mundo? ")
+console.log("A - Carneiro Texel")
+console.log("B - Pato Big Dave")
+console.log("C - Panda-gigante")
+console.log("D - Cavalo Fusaichi Pegasus")
+let pergunta10 = prompt("Digite a alternativa correta: ")
+if (pergunta10 == "D" || pergunta10 == "Cavalo Fusaichi Pegasus"){
+  console.log("Alternativa correta! ")
+  respostaCorreta ++
+} else {
+  console.log("Alternativa Errada! ")
+  respostaIncorreta ++
+}
+
+//Exibir Resultado
+console.log("\n Resumo Final")
+console.log(" respostas Correta: " + respostaCorreta)
+console.log(" respostas Incorreta: " + respostaIncorreta)
