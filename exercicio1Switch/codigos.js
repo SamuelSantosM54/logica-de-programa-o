1fbbfd72a1ee76cca1c9d7dfbe9b91bbb751c9a9
SamuelS <samucalexx@gmail.com>
2025-06-03 10:48:29 -0300

    codigos.js

---
+++ exercicio1Switch/codigos.js
@@ -0,0 +1,239 @@
+console.log("Qual seu idioma? ")
+console.log("A - Espanhol")
+console.log("B - Portugues")
+console.log("C - Francês")
+let pergunta1 = prompt("Digite a alternativa: ")
+switch(pergunta1) {
+case "A":
+console.log("BUENAS NOCHES")
+break
+case "B":
+console.log("Boa Noite")
+break
+case "C":
+console.log("BONNE NUIT")
+}
+
+console.log("Qual o dia da semana? ")
+console.log("20")
+console.log("21")
+console.log("22")
+let pergunta2 = prompt("Qual a data: ")
+switch(pergunta2) {
+case "20":
+console.log("Sexta-feira")
+break
+case "21":
+console.log("Sábado")
+break
+case "22":
+console.log("Domingo")
+}
+
+console.log("Veículos ")
+console.log("1")
+console.log("2")
+console.log("3")
+let pergunta3 = prompt("Qual veículo você quer ver: ")
+switch(pergunta3) {
+case "1":
+console.log("Carro")
+break
+
+case "2":
+console.log("Onibus")
+break
+case "3":
+console.log("Bicicleta")
+}
+
+console.log("Nivel de Urgência ")
+console.log("1")
+console.log("2")
+console.log("3")
+let pergunta4 = prompt("Escolha um nível: ")
+switch(pergunta4) {
+case "1":
+console.log("Baixo")
+break
+case "2":
+console.log("Médio")
+break
+case "3":
+console.log("Alta")
+}
+
+console.log("Fases da lua ")
+console.log("4")
+console.log("12")
+console.log("20")
+let pergunta5 = prompt("Escolha o dia do mês de abril: ")
+switch(pergunta5) {
+case "4":
+console.log("Lua crescente")
+break
+case "12":
+console.log("Lua cheia")
+break
+case "20":
+console.log("Lua minguante")
+}
+
+console.log("Tipos de carros ")
+console.log("Renault Kwid ")
+console.log("Jeep Renegade")
+console.log("Volvo FH")
+let pergunta6 = prompt("Escolha uma marca: ")
+switch(pergunta6) {
+case "A - Renault Kwid":
+console.log("Carro Compacto")
+break
+case "B - Jeep Renegade":
+console.log("SUV")
+break
+case "C - Volvo FH":
+console.log("Caminhão")
+}
+console.log("Tarifas do estacionamento ")
+console.log("2 horas")
+console.log("4 horas")
+console.log("8 horas")
+let pergunta7 = prompt("Escolha um tempo estacionado: ")
+switch(pergunta7) {
+case "2 horas":
+console.log("R$5,00 ")
+break
+case "4 horas":
+console.log("R$10,00 ")
+break
+case "8 horas":
+console.log("R$20,00 ")
+}
+
+console.log("Nivel de dificuldade no jogo ")
+console.log("1")
+console.log("2")
+console.log("3")
+let pergunta8 = prompt("Escolha um nível: ")
+switch(pergunta8) {
+case "1":
+console.log("Fácil ")
+break
+case "2":
+console.log("Médio ")
+break
+case "3":
+console.log("Difícil ")
+
+}
+
+console.log("Tipos de serviços ")
+console.log("Plano Essencial")
+console.log("Plano Extra")
+console.log("Plano Deluxe")
+let pergunta9 = prompt("Escolha um nível: ")
+switch(pergunta9) {
+case "Plano Essencial":
+console.log("Musculação")
+break
+case "Plano Extra":
+console.log("Yoga")
+break
+case "Plano Deluxe":
+console.log("Personal trainer")
+}
+console.log("Tipos de ingressos ")
+console.log("Estudante")
+console.log("Meia entrada")
+console.log("Inteira")
+let pergunta10 = prompt("Escolha um tipo: ")
+switch(pergunta10) {
+case "Estudante":
+console.log("R$15,00")
+break
+case "Meia entrada":
+console.log("R$21,76")
+break
+case "Inteira":
+console.log("R$38,76")
+}
+
+console.log("Dificuldade em exercícios físicos ")
+console.log("18+")
+console.log("30+")
+console.log("60+")
+let pergunta11 = prompt("Escolha uma idade: ")
+switch(pergunta11) {
+case "18+":
+console.log("Fácil")
+break
+case "30+":
+
+console.log("Médio")
+break
+case "60+":
+console.log("Difícil")
+}
+
+console.log("Gêneros musicais com base nos instrumentos")
+console.log("Sanfona")
+console.log("Violino")
+console.log("Inteira")
+let pergunta12 = prompt("Escolha um instrumento: ")
+switch(pergunta12) {
+case "Sanfona":
+console.log("Forró")
+break
+case "Violino":
+console.log("Country")
+}
+
+console.log("Cursos de tecnologia")
+console.log("Front-end")
+console.log("Back-end")
+console.log("Mobile")
+let pergunta13 = prompt("Escolha um curso: ")
+switch(pergunta13) {
+case "Front-end":
+console.log("É o que os usuários veem, como botões, caixas de seleção, gráficos e
+mensagens de texto ")
+break
+case "Back-end":
+console.log("É a parte dos bastidores, onde toda a lógica e processamento
+acontecem ")
+break
+case "Mobile":
+console.log("É a área de desenvolvimento de aplicativos para celulares e tablets")
+}
+
+console.log("Nível de habilidade em esportes com base na faixa etária")
+console.log("10+")
+console.log("20+")
+console.log("50+")
+let pergunta14 = prompt("Escolha uma faixa etária: ")
+switch(pergunta14) {
+case "10+":
+console.log("Alta")
+break
+case "20+":
+console.log("Muito Alta")
+break
+case "50+":
+console.log("Baixa")
+}
+
+console.log("Nível de dificuldade em Matématica ")
+console.log("A hipótese de Riemann")
+console.log("Frações e suas operações")
+console.log("Trigonometria")
+let pergunta15 = prompt("Escolha um assunto: ")
+switch(pergunta15) {
+case "A hipótese de Riemann":
+console.log("Avançado")
+break
+case "Frações e suas operações":
+console.log("Básico")
+break
+case "Trigonometria":
+console.log("Intermediário")
+}
