0a15984a42d2063f5798049f802dca2bd0a4a5ec
SamuelS <samucalexx@gmail.com>
2025-06-03 10:29:53 -0300

    codigos.js

---
+++ linguagemNatural/codigos.js
@@ -0,0 +1,44 @@
+Como fazer um arroz?
+Lave o arroz (aproximadamente 1 xícara) até a água sair clara, refogue o arroz com um pouco
+de óleo ou alho em uma panela média, adicione 2 xícaras de água (ou o dobro da quantidade
+de arroz), tempere com sal a gosto, deixe ferver. Quando a água começar a ferver, reduza o
+fogo para baixo e tampe a panela, cozinhe por 15 a 20 minutos, até a água secar, desligue o
+fogo e deixe o arroz descansar tampado antes de servir.
+
+Como fazer um chá?
+Ferva a água, coloque o saquinho de chá em uma xícara, despeje a água quente sobre o
+saquinho por 3-5 minutos, coe as ervas e retire o saquinho, e enfim adoce o chá.
+
+Preparar um sanduíche?
+Pegue duas fatias de pão, adicione o recheio (como queijo, presunto, alface, tomate ou o que
+preferir) e feche o pão.
+
+Compra um ingresso online?
+Acesse o site ou app, escolha o evento e a data, selecione o assento ou tipo de ingresso,
+adicione ao carrinho e vá para o pagamento, insira seus dados e finalize a compra, receba o
+ingresso por e-mail ou app.
+
+Organizar a mochila para a escola?
+Separe os materiais (cadernos, livros, estojos), coloque os itens mais pesados no fundo, como
+livros e cadernos, adicione itens pessoais como celular, carteira e feche a mochila.
+
+Arrumar a cama?
+Estique o lençol sobre o colchão, alisando os cantos, coloque o cobertor ou edredom e estique
+bem, alise o travesseiro e coloque-o na cabeceira da cama.
+
+Fazer um suco?
+Escolha a fruta e corte-a, coloque a fruta no liquidificador com água e, se quiser adicione
+açúcar, bata bem até ficar homogêneo, coe se preferir sem polpa e sirva.
+
+Lavar a louça?
+Lave os pratos, copos, talheres com sabão e água, começando pelos menos sujos, enxágue bem
+e coloque para secar ou seque com um pano.
+
+Troca uma lâmpada?
+Desligue a energia no disjuntor ou no interruptor, retire a lâmpada queimada com cuidado,
+girando-a, coloque a lâmpada nova no soquete e gire até fixar, ligue a energia e verifique se a
+lâmpada acende.
+
+Enviar uma mensagem no celular?
+Abra o aplicativo de mensagens, selecione o contato, digite sua mensagem no campo
+de texto, (geralmente um avião de papel ou seta).
