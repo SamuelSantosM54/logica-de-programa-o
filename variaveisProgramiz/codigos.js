e1845f595794c74124eba4969da184e2f14f24cc
SamuelS <samucalexx@gmail.com>
2025-06-03 10:33:02 -0300

    codigos.js

---
+++ variaveisProgramiz/codigos.js
@@ -0,0 +1,27 @@
+let nome = &quot;Samuel&quot;
+console.log(nome)
+let idade = 16
+console.log(idade)
+let aprendendoJavaScript = true
+console.log(aprendendoJavaScript)
+let nome2 = &quot;Samuel&quot;
+let sobrenome = &quot;Santos&quot;
+let nomecompleto = nome2 + &quot;&quot; + sobrenome
+console.log(nomecompleto)
+let num1 = 10
+let num2 = 20
+console.log(num1 + num2)
+let num3 = 30
+let num4 = 30
+console.log(num3*num4)
+let cidade = &quot;São Paulo&quot;
+let habitantes = 12300000
+console.log(&quot;Na cidade de&quot;,cidade, &quot;tem&quot;, habitantes, &quot;milhões de habitantes&quot;)
+let preco = 100
+console.log(&quot;100: &quot; + preco)
+preco = 120
+console.log(&quot;120: &quot; + preco)
+let meuValor
+console.log(meuValor)
+let meuNulo = null
+console.log(meuNulo)
