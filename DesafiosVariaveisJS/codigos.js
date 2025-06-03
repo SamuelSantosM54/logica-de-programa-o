6749f6fcc4c95224556c20baef2f96bee21bf23e
SamuelS <samucalexx@gmail.com>
2025-06-03 10:36:32 -0300

    codigos.js

---
+++ DesafiosVariaveisJS/codigos.js
@@ -0,0 +1,13 @@
+let nome = prompt("Digite seu nome: ")
+let idade = Number(prompt("Digite a sua idade: "))
+let email = prompt("Digite seu email: ")
+let telefone = prompt("Digite seu telefone: ")
+console.log("Pronto!seu cadastro foi realizado com sucesso")
+
+
+
+let anoNascimento = prompt("Digite o seu ano de nascimento:")
+anoNascimento = Number(anoNascimento)
+let anoAtual = 2025
+let idade = anoAtual - anoNascimento
+alert("Sua idade em 2025 será: " + idade + " anos.")
