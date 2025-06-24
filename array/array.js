d530b5ef15e90dd1d19dcdeeec135beeaf1207bc
SamuelS <samucalexx@gmail.com>
2025-06-24 14:30:54 -0300

    array

---
+++ array/array.js
@@ -0,0 +1,72 @@
+let carros = ["Corolla", "Civic", "Fiat Cronos"];
+console.log(carros);
+
+
+let numeros = [10, 20, 30];
+console.log(numeros);
+
+
+let nomes = ["Samuel", "Lucas", "Fernando"];
+console.log(nomes);
+
+
+let cores = ["azul", "verde", "vermelho"];
+console.log(cores[0]);
+
+
+let frutas = ["banana", "maçã", "uva", "laranja"];
+console.log(frutas[2]);
+
+
+
+let idades = [15, 25, 30, 18];
+console.log(idades[3]);
+
+
+let animais = ["gato", "cachorro", "peixe"];
+console.log("O primeiro animal é:", animais[0]);
+console.log("O segundo animal é:", animais[1]);
+
+
+let notas = [8, 7, 9, 6];
+console.log("A primeira nota é:", notas[0]);
+console.log("A última nota é:", notas[notas.length - 1]);
+
+
+let cidades = ["São Paulo", "Rio de Janeiro", "Salvador", "Recife"];
+console.log("A segunda cidade é:", cidades[1]);
+console.log("A quarta cidade é:", cidades[3]);
+
+
+let times = ["Botafogo", "Fluminense", "Flamengo"];
+times.push("Vasco");
+console.log(times);
+
+
+let produtos = ["notebook", "mouse", "teclado"];
+produtos[0] = "celular";
+console.log(produtos);
+
+
+let bebidas = ["água", "suco", "refrigerante"];
+bebidas[bebidas.length - 1] = "café";
+console.log(bebidas);
+
+
+let lista = [];
+lista[0] = "primeiro";
+console.log(lista);
+
+
+let valores = [];
+valores[0] = 100;
+valores[1] = 200;
+console.log(valores);
+
+
+let meuArray = [];
+meuArray[0] = "Samuel Santos";
+meuArray[1] = 30; 
+meuArray[2] = "Ferraz de Vasconcelos"; 
+console.log(meuArray);
+
